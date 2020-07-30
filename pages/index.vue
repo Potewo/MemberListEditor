ef495f1194adfe5daf17128ce7d9c432146571cd
Potewo <potewo@protonmail.com>
2020-07-30 17:10:58 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <v-text-field v-model="name" label="Name" />
+    <v-text-field v-model="codeNumber" label="Code Number" minlength="4" maxlength="4" />
+    <v-btn @click="send">
+      Send
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '',
+      codeNumber: ''
+    }
+  },
+  methods: {
+    send () {
+      alert(this.name + '\n' + this.codeNumber)
+      this.name = ''
+      this.codeNumber = ''
+    }
+  }
+}
+</script>
